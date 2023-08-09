0361ef716591346da4fe45f1684c8841268a046c
bhusalanil121 <bhusalanil121@gmail.com>
2023-08-09 12:37:28 -0400

    spring security

--- Microservices-User-Resturent/HotelService/src/main/java/com/hotel/controllers/HotelController.java
+++ Microservices-User-Resturent/HotelService/src/main/java/com/hotel/controllers/HotelController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -23,7 +24,8 @@ public class HotelController {
 	private HotelServices hotelService;
 	
 	
-	//create 
+	//create
+	@PreAuthorize("hasAuthority('Admin')")
 	@PostMapping
 	public ResponseEntity<Hotel> createHotel(@RequestBody Hotel hotel){
 		return ResponseEntity.status(HttpStatus.CREATED).body(hotelService.create(hotel));
@@ -32,12 +34,14 @@ public class HotelController {
 	
 	
 	//get single
+	@PreAuthorize("hasAuthority('SCOPE_internal')")
 	@GetMapping("/{hotelId}")
 	public ResponseEntity<Hotel> createHotel( @PathVariable String hotelId){
 		return ResponseEntity.status(HttpStatus.OK).body(hotelService.get(hotelId));
 	}
 	
 	//getAll
+	@PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
 	@GetMapping
 	public ResponseEntity<List<Hotel>> getAllHotel(){
 		return ResponseEntity.status(HttpStatus.OK).body(hotelService.getAllHotel());
--- Microservices-User-Resturent/RatingService/src/main/java/com/rating/controller/RatingController.java
+++ Microservices-User-Resturent/RatingService/src/main/java/com/rating/controller/RatingController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,13 +23,14 @@ public class RatingController {
 	
 	@Autowired
 	private RatingService ratingService;
-	
+	@PreAuthorize("hasAuthority('Admin')")
 	@PostMapping
 	public ResponseEntity<Rating> create(@RequestBody Rating rating){
 		return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.create(rating));
 		
 	}
-	
+	//get all user
+	@PreAuthorize("hasAuthority('SCOPE_internal') || hasAuthority('Admin')")
 	@GetMapping
 	public ResponseEntity<List<Rating>> getRatings(){
 		return ResponseEntity.ok(ratingService.getRatings());
